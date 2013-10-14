16d67b2e45124db3e39dd4a2aaee3ac1126eb5fc
Lars Sjöström <lars.sjostrom@svenskaspel.se>
2013-10-14 14:05:17 +0200

    clean up eventserver

--- flumelogger/eventserver.py
+++ flumelogger/eventserver.py
@@ -2,20 +2,11 @@
 
 from thrift.transport import TTransport
 from thrift.transport import TSocket
-from thrift.transport import THttpClient
 from thrift.protocol import TCompactProtocol
 from flumelogger.flumeng import ThriftSourceProtocol
 from flumelogger.flumeng.ttypes import ThriftFlumeEvent
 
 class FlumeEventServer(object):
-    PRIORITY = { "FATAL"   : 0,
-                 "CRITICAL": 0,
-                 "ERROR"   : 1,
-                 "WARNING" : 2,
-                 "INFO"    : 3,
-                 "DEBUG"   : 4,
-                 "TRACE"   : 5 }
-
     def __init__(self, host="localhost", port=9090):
         self.socket = TSocket.TSocket(host, port)
         self.socket._timeout = 1000
@@ -29,7 +20,6 @@ class FlumeEventServer(object):
             headers = headers,
             body = body,
         )
-        print event
         self.client.append(event)
 
     def close(self):
