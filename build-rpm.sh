dcfd26d7634e7cb9dba7fde4ff6656de36351239
Lars Sjöström <lars.sjostrom@svenskaspel.se>
2013-10-15 16:33:14 +0200

    bump version

--- build-rpm.sh
+++ build-rpm.sh
@@ -1,1 +1,1 @@
-fpm -s dir -t rpm -n python-flumelogger -v 0.2 -a noarch --prefix /usr/lib/python2.6/site-packages -d python-thrift flumelogger
+fpm -s dir -t rpm -n python-flumelogger -v 0.3 -a noarch --prefix /usr/lib/python2.6/site-packages -d python-thrift flumelogger
